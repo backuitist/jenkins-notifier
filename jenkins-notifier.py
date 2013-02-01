ac5739388f066d51bb4348537e7b92d7c3755aee
Bruno Bieth <biethb@gmail.com>
2013-02-01 11:36:19 +0100

    fixed incorrect api usage

--- jenkins-notifier.py
+++ jenkins-notifier.py
@@ -8,6 +8,8 @@ pygtk.require('2.0')
 import gtk
 import gobject
 import os
+import sys
+import traceback
 
 class JenkinsNotifier:
 	JOB = 'Backuity'
@@ -18,18 +20,26 @@ class JenkinsNotifier:
 	UNKNOWN_IMG = os.path.abspath(dir_path + '/jenkins-grey.png')
 	statusIconImg = UNKNOWN_IMG
 	statusIcon = gtk.StatusIcon()
+	notification = None
+	lastKnownBuild = None
 
 	def notifySuccess(self,msg): 
-		n = pynotify.Notification(self.JOB, 
+		self.closeNotification()
+		self.notification = pynotify.Notification(self.JOB, 
 			msg, self.SUCCESS_IMG) 
-		n.set_urgency(pynotify.URGENCY_LOW) 
-		n.show() 
+		self.notification.set_urgency(pynotify.URGENCY_LOW) 
+		self.notification.show() 
 
-	def notifyFailure(self,msg): 
-		n = pynotify.Notification(self.JOB, 
+	def closeNotification(self):
+		if self.notification is not None:
+			self.notification.close()
+
+	def notifyFailure(self,msg):
+		self.closeNotification() 
+		self.notification = pynotify.Notification(self.JOB, 
 			msg, self.FAILURE_IMG) 
-		n.set_urgency(pynotify.URGENCY_CRITICAL) 
-		n.show() 
+		self.notification.set_urgency(pynotify.URGENCY_CRITICAL) 
+		self.notification.show() 
 
 	def updateStatusIcon(self, icon, msg):
 		self.statusIconImg = icon
@@ -38,33 +48,44 @@ class JenkinsNotifier:
 
 	def success(self,lastBuild):
 		if self.statusIconImg != self.SUCCESS_IMG:
-			msg = 'Build #' + lastBuild + ' is a Success'
+			msg = 'Build #' + str(lastBuild) + ' is a Success'
+			print msg
 			self.updateStatusIcon(self.SUCCESS_IMG, msg)
 			self.notifySuccess(msg)
 
 	def failure(self,lastBuild):
-		if self.statusIconImg != self.FAILURE_IMG:
-			msg = 'Build #' + str(lastBuild) + ' Failed!'
-			self.updateStatusIcon(self.FAILURE_IMG, msg)
-			self.notifyFailure(msg)
+		msg = 'Build #' + str(lastBuild) + ' Failed!'
+		print msg
+		self.updateStatusIcon(self.FAILURE_IMG, msg)
+		self.notifyFailure(msg)
 
 	def refresh(self):
-		feed = eval(urllib.urlopen(self.URL + '/api/python').read())
-		lastBuild = feed['lastBuild']['number']
-		if lastBuild != self.lastKnownBuild:
-			self.lastKnownBuild = lastBuild			
-			lastSuccess = feed['lastSuccessfulBuild']
-			if lastSuccess == None or lastSuccess['number'] != lastBuild:
-				self.failure(lastBuild)
-			else:
-				self.success(lastBuild)
-		# return true to keep calling that function, see gobject.timeout_add
-		return True
+		try:
+			# print "refreshing, lastKnownBuild is ", self.lastKnownBuild
+			feed = eval(urllib.urlopen(self.URL + '/api/python').read())
+			lastBuild = feed['lastCompletedBuild']
+			# print "lastBuild is now ", lastBuild
+			if lastBuild is not None:
+				lastBuild = lastBuild['number']
+
+				if lastBuild != self.lastKnownBuild:
+
+					self.lastKnownBuild = lastBuild			
+					lastSuccess = feed['lastSuccessfulBuild']
+					# print "Last success", lastSuccess
+					if lastSuccess is None or lastSuccess['number'] != lastBuild:
+						self.failure(lastBuild)
+					else:
+						self.success(lastBuild)
+				# return true to keep calling that function, see gobject.timeout_add
+			return True
+		except:
+			traceback.print_exc(file=sys.stdout)
+			sys.exit()
 
 	def __init__(self):
 		pynotify.init('Jenkins Notify') 
 		self.updateStatusIcon(self.UNKNOWN_IMG, 'Connecting to Jenkins ...')
-		self.lastKnownBuild = -1
 		gobject.timeout_add(3000, self.refresh)
 		gtk.main()
 
